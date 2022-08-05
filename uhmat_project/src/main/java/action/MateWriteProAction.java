0e4219fcb38810228fa3123d730bfac41c62b9f0
imzooyoung <108872288+imzooyoung@users.noreply.github.com>
2022-08-05 23:36:06 +0900

    22.08.05 (#10)
    * 22.08.04 10:18
    
    * 22.08.05 12:19 GIT COMMIT 연습중입니다.
    
    * 22.08.05 연습해봅니다!
    
    * 22.08.05 임주영 작업
    
    Co-authored-by: imzooyoung <dlawn2220@gmail.com>

--- uhmat_project/src/main/java/action/MateWriteProAction.java
+++ uhmat_project/src/main/java/action/MateWriteProAction.java
@@ -14,7 +14,7 @@ public class MateWriteProAction implements Action {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("MateWriteProAction");
-		
+
 		// 포워딩 정보를 저장하는 ActionForward 타입 변수 선언
 		ActionForward forward = null;
 		
@@ -23,7 +23,6 @@ public class MateWriteProAction implements Action {
 		mate.setNickname(request.getParameter("nickname"));
 		mate.setSubject(request.getParameter("subject"));
 		mate.setContent(request.getParameter("content"));
-		
 		
 		MateWriteProService service = new MateWriteProService();
 		// 실제 비즈니스 작업 요청을 수행할 MateWriteProService 클래스의 인스턴스 생성 후
@@ -49,7 +48,6 @@ public class MateWriteProAction implements Action {
 			forward.setPath("MateList.mate");
 			forward.setRedirect(true);
 		}
-		
 		
 		return forward;
 	}
