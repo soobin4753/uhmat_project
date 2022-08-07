6e6313f97e54fc7043b4694474d3037f5dac9d72
soobin4753 <107300167+soobin4753@users.noreply.github.com>
2022-08-07 16:29:02 +0900

    어맛메이트 글쓰기 + 리스트 (#16)
    * 주영이 오빠한테 넘기는 communityFrontController
    
    * 어맛메이트 글쓰기 + 리스트 뿌리기
    
    * 커뮤니티 어맛메이트 디테일 + 조회수
    
    Co-authored-by: anseungwoo <73145138+anseungwoo@users.noreply.github.com>

--- uhmat_project/src/main/java/action/MateWriteProAction.java
+++ uhmat_project/src/main/java/action/MateWriteProAction.java
@@ -19,10 +19,11 @@ public class MateWriteProAction implements Action {
 		ActionForward forward = null;
 		
 		MateDTO mate = new MateDTO();
-		mate.setIdx(Integer.parseInt(request.getParameter("idx")));
+//		mate.setIdx(Integer.parseInt(request.getParameter("idx")));
 		mate.setNickname(request.getParameter("nickname"));
 		mate.setSubject(request.getParameter("subject"));
 		mate.setContent(request.getParameter("content"));
+		System.out.println(mate);
 		
 		MateWriteProService service = new MateWriteProService();
 		// 실제 비즈니스 작업 요청을 수행할 MateWriteProService 클래스의 인스턴스 생성 후
