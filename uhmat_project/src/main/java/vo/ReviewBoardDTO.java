9dc723a2c471f81a9e1d31afd7d5927bab7682b2
anseungwoo <73145138+anseungwoo@users.noreply.github.com>
2022-08-07 16:32:34 +0900

    Kaleb (#18)
    * deletehelloworld (#12)
    
    * add- restaurantCategory, reviewList (#13)
    
    * deletehelloworld
    
    * 220806 change-xml resource uhmat, add - index RestaurantCategory, add -
    
    Co-authored-by: anseungwoo <73145138+anseungwoo@users.noreply.github.com>
    
    Co-authored-by: kaleb96 <97508453+kaleb96@users.noreply.github.com>

---
+++ uhmat_project/src/main/java/vo/ReviewBoardDTO.java
@@ -0,0 +1,86 @@
+package vo;
+
+/*
+CREATE TABLE reviewBoard(
+idx INT PRIMARY KEY,
+res_name VARCHAR(50) REFERENCES restaurantInfo(res_name),
+nickname VARCHAR(10) REFERENCES member(id),
+subject VARCHAR(50) NOT NULL,
+photo VARCHAR(50),
+content VARCHAR(5000) NOT NULL,
+likes INT DEFAULT 0,
+rating FLOAT DEFAULT 0
+);
+ */
+
+public class ReviewBoardDTO {
+	private int idx;
+	private String res_name;
+	private String nickname;
+	private String subject;
+	private String photoName;
+	private String content;
+	private int likes;
+	private float rating;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getRes_name() {
+		return res_name;
+	}
+	public void setRes_name(String res_name) {
+		this.res_name = res_name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getPhotoName() {
+		return photoName;
+	}
+	public void setPhotoName(String photoName) {
+		this.photoName = photoName;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getLikes() {
+		return likes;
+	}
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	public float getRating() {
+		return rating;
+	}
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+	@Override
+	public String toString() {
+		return "ReviewBoardDTO [idx=" + idx + ", res_name=" + res_name + ", nickname=" + nickname + ", subject="
+				+ subject + ", photoName=" + photoName + ", content=" + content + ", likes=" + likes + ", rating="
+				+ rating + "]";
+	}
+	
+
+	
+	
+	
+	
+}
--- uhmat_project/src/main/webapp/index.jsp
+++ uhmat_project/src/main/webapp/index.jsp
@@ -8,14 +8,22 @@
 </head>
 <body>
  
-<h3>hello</h3> <h3>World</h3> <h3>!</h3>
 
 <h3> <a href="MemberJoinFrom.me">회원가입</a></h3>
 <h3> <a href="MemberLoginFrom.me">로그인</a></h3>
+ <nav>
+ 	<details> <!-- 음식카테고리 리스트 -->
+ 		<summary>음식카테고리</summary>
+ 		<a href="RestaurantInfo.re">음식으로 보기</a>
+ 		<a href="ReviewList.re">리뷰로 보기</a>
+ 		<a href="#">지도로 보기</a>
+ 	</details>
+ </nav>
 <h3> <a href="NoticeList.sc">Notice</a></h3>
 <h3> <a href="FAQList.sc">FAQ</a></h3>
  <h3><a href="MateWriteForm.mate">글쓰기</a></h3>
 <h3><a href="MateList.mate">글목록</a></h3>
+
 
 </body>
 </html>   
