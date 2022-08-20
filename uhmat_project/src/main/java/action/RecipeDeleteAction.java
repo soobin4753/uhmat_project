577a46e3a12672659dd1053d38e94c93ae4c310d
soobin4753 <soobin4753@naver.com>
2022-08-20 14:46:44 +0900

    실제 recipe_upload에 강제로 이미지 이름을 넣어 실제경로에서 삭제되는 것 까지만 확인

--- uhmat_project/src/main/java/action/RecipeDeleteAction.java
+++ uhmat_project/src/main/java/action/RecipeDeleteAction.java
@@ -1,12 +1,19 @@
 package action;
 
+import java.io.File;
 import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jasper.tagplugins.jstl.core.Param;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 import svc.RecipeDeleteService;
 import vo.ActionForward;
+import vo.RecipeDTO;
 
 public class RecipeDeleteAction implements Action {
 
@@ -20,8 +27,14 @@ public class RecipeDeleteAction implements Action {
 		String nickname = request.getParameter("nickname");
 		System.out.println("idx : " + idx);
 		
+		String file1 = request.getParameter("file1");
+		System.out.println("file1 : " + file1);
+		
+		String filePath = "D:\\workspace_jsp1\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\uhmat_project\\recipe_upload" + file1;
+		File deleteFile = new File(filePath);
+		
 		// RecipeDeleteProService - isDeleteMate() 메서드를 호출하여 삭제 요청
-		// => 파라미터 : 글번호    리턴타입 : boolean(isDeleteSuccess)
+		// => 파라미터 : 글번호, 닉네임    리턴타입 : boolean(isDeleteSuccess)
 		RecipeDeleteService service = new RecipeDeleteService();
 		boolean isDeleteSuccess = service.deleteRecipe(idx, nickname);
 		
@@ -37,9 +50,20 @@ public class RecipeDeleteAction implements Action {
 			out.println("</script>");
 		} else { // 게시글 삭제 성공 
 			
-			forward = new ActionForward();
-			forward.setPath("RecipeList.co?pageNum=" + request.getParameter("pageNum"));
-			forward.setRedirect(true);
+			if(deleteFile.exists()) {
+				deleteFile.delete();
+				System.out.println("파일 삭제");
+				
+				forward = new ActionForward();
+				forward.setPath("RecipeList.co?pageNum=" + request.getParameter("pageNum"));
+				forward.setRedirect(true);
+				
+			} else {
+				System.out.println("파일이 존재하지 않습니다");
+			}
+				
+				
+			
 			
 		}
 		return forward;
--- uhmat_project/src/main/webapp/community/recipe/recipe_delete.jsp
+++ uhmat_project/src/main/webapp/community/recipe/recipe_delete.jsp
@@ -7,7 +7,9 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<form action="RecipeDeletePro.co?idx=${param.idx }&pageNum=${param.pageNum}&nickname=${param.nickname}" name="deleteForm" method="post">
+	<form action="RecipeDeletePro.co?idx=${param.idx }&pageNum=${param.pageNum }&nickname=${param.nickname }" name="deleteForm" method="post">
+<%-- 	<input type="hidden" name="file1" value="${recipe.real_File1 }"> --%>
+	
 	<h1>글 삭제 하시겠습니까?</h1>
 <!-- 	<input type="password" placeholder="패스워드를 적으시오"> -->
 	<input type="submit" value="삭제" >&nbsp;&nbsp;
